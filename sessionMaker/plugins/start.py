0ab7fc06a4949dbf592d1f8e80241738cfb4d092
zoeyzsz <zoeyzsz@noreply.github.com>
2021-06-11 23:35:12 +0700

    Update start.py

--- sessionMaker/plugins/start.py
+++ sessionMaker/plugins/start.py
@@ -7,14 +7,14 @@ from pyrogram.types import (
 from sessionMaker import sessionCli
 
 START_MESSAGE = (
-        'Hello there!\n'
-        'I can generate session of [pyrogram](https://t.me/pyrogram) and [telethon](https://t.me/TelethonUpdates).\n\n'
-        '**Note**: We don\'t collect or log users\' credentials.\n'
-        'If something happens to your account, we aren\'t responsible for it.\n\n'
-        'If you want to host your own bot then here is the github repo under the terms of the [GNU General Public License v3.0](https://github.com/meanii/sessionMaker/blob/master/LICENSE).\n'
-        '> https://github.com/meanii/sessionMaker\n\n'
-        'News channel: @spookyanii\n'
-        'Support chat: @nina77chat' 
+        'Hai!\n'
+        'I can generate Session String **Pyrogram & Telethon** for your UserBot.\n\n'
+    
+        '**This requires :**\n- **API ID**\n- **API HASH**\n- **PHONE NUMBER**\n- **VERIFICATION**\n\nI suggest you to prepare the required items before using this Bot'
+    
+        '**Note** : I do not collect or log your login credentials/sessions.\n'
+        'Bot By : @hilmay619\n\n'
+        '**Now, choose the one you need**'
     )
 
 KEYBOARD = InlineKeyboardMarkup(
