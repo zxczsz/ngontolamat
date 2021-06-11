b900f3843f2b31dcd7bc71206dc91f7770f12446
zoeyzsz <zoeyzsz@noreply.github.com>
2021-06-11 23:12:28 +0700

    update

---
+++ environment.py
@@ -0,0 +1,5 @@
+class BasicSettings:
+    API_ID: int=  1765075
+    API_HASH: str = "af3ca111ad7d31c6ea900d7923b43dd7"
+    BOT_TOKEN: str = "1733137120:AAFsxuxFzqWDfMdQWk-zIIVPUgku_6lD6vs"
+    LOG_CHANNEL: int = "-1001347703925"
---
+++ sessionMaker/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from heroku3 import from_key
+from pyrogram import Client
+from pyromod import listen
+
+from sessionMaker.environment import BasicSettings
+
+# Init bot session
+API_ID = BasicSettings.API_ID
+API_HASH = BasicSettings.API_HASH
+BOT_TOKEN = BasicSettings.BOT_TOKEN
+LOG_CHANNEL = BasicSettings.LOG_CHANNEL
+
+sessionCli = Client(
+    'botSession',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(
+        root='sessionMaker.plugins'
+    )
+)
